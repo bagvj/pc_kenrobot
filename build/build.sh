04e99c35d88c8a37f4c1bf6b1367c327ba59b56f
Lishinian <lishinian@kenrobot.com>
2017-01-18 16:28:33 +0800

    mofify mac pack script

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 title='啃萝卜' # dmg文件mount了之后在文件系统中显示的名称
-background_picture_name='mac_bg.png' # dmg文件在mount了之后界面中显示的背景图片路径
+background_picture_name='mac_bg.jpg' # dmg文件在mount了之后界面中显示的背景图片路径
 application_name='啃萝卜.app' # 应用程序的名称
 size='370M' # 大小
 product_name='kenrobot' # 产品名
@@ -37,6 +37,7 @@ function ejectDmgMount() {
 function setDmgFinderInfo() {
   window_right=$(($image_width+$window_left))
   window_bottom=$(($image_height+$window_top))
+  echo 555666
   echo '
      tell application "Finder"
        tell disk "'${title}'"
@@ -49,12 +50,12 @@ function setDmgFinderInfo() {
              set arrangement of theViewOptions to not arranged
              set icon size of theViewOptions to '$app_icon_width'
              set background picture of theViewOptions to file ".background:bg.png"
-             set position of item "'${application_name}'" of container window to {'$app_icon_left', '$app_icon_top'}
              update without registering applications
              close
        end tell
      end tell
   ' | osascript
+  echo 5556667777
 }
 
 # 打包dmg
@@ -77,12 +78,16 @@ echo begin build dmg
 mkdir -p ${output_dir}
 rm -f ${output_dir}/${output_name}
 hdiutil create -size ${size} -volname "${title}" -fs HFS+ -fsargs "-c c=64,a=16,e=16" ${output_dir}/${output_name}
+echo 11111
 
 # 如果有 mount 了其他的 dmg 文件在 Finder 里面了，先弹出掉
 if [ -d /Volumes/${title} ]; then
+  echo 1111222
   ejectDmgMount
 fi
+echo 2222
 hdiutil mount ${output_dir}/${output_name}
+echo 3333
 
 image_width=`sips -g pixelWidth ${background_picture_name} | tail -n 1 | grep -oE '[0-9]+$'`
 image_height=`sips -g pixelHeight ${background_picture_name} | tail -n 1 | grep -oE '[0-9]+$'`
@@ -90,11 +95,14 @@ image_height=`sips -g pixelHeight ${background_picture_name} | tail -n 1 | grep 
 # 复制编译好的app目录
 rm -rf /Volumes/${title}/${app_folder_name}
 cp -R ${app_folder_name} /Volumes/${title}/${app_folder_name}
+echo 4444
 
 mkdir -p /Volumes/${title}/.background
 rm -f /Volumes/${title}/.background/*
 cp ./${background_picture_name} /Volumes/${title}/.background/bg.png
+echo 5555
 
 buildDmg
+echo 6666
 rm -rf ./${output_dir}/${output_name}
 echo build success
