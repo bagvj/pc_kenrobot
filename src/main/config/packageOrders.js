b7bbc1f11767ede2c6a97eb4a5c57ab566d0c89b
zhouguang <zhouguang@kenrobot.com>
2018-01-10 16:16:17 +0800

    fix bug

--- src/main/config/packageOrders.js
+++ src/main/config/packageOrders.js
@@ -1,6 +1,6 @@
 module.exports = {
 	/*
-     * 包顺序。假如有3个包A、B、C，其中A没有定义order，B.order < C.order，那么排完之后是C、B、A
+     * 包顺序。假如有3个包A、B、C，其中A没有定义order，B.order < C.order，那么排完之后是B、C、A
 	 */
 	Arduino: -999,
 	Boxz: 10,
