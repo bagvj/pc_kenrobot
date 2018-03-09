bfe387e40c169d734c428a6282f858f1b08b4033
zhouguang <zhouguang@kenrobot.com>
2018-03-09 12:14:20 +0800

    opt && fix bugs

--- src/renderer/assets/js/app/common/config/config.js
+++ src/renderer/assets/js/app/common/config/config.js
@@ -2,7 +2,7 @@ define(function() {
 	var configs = {
 		//基本配置
 		url: {
-			kenrobot: "http://www.kenrobot.com",
+			kenrobot: "https://www.kenrobot.com",
 			arduino: "http://www.arduino.cn",
 			support: "http://www.arduino.cn/forum-101-1.html",
 			about: "http://www.kenrobot.com/index.php?app=square&mod=Index&act=help",
